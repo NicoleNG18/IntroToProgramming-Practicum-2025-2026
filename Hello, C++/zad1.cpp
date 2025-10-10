61b34d05dbdc2b39304d39fd5371380ffb6d5e68
sarajelenkov <162555011+sarajelenkov@users.noreply.github.com>
2025-10-10 14:14:21 +0300

    Add files via upload

---
+++ Hello, C++/zad1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, c++";
+    int apples, pears, bananas;
+    std::cin >> apples >> pears >> bananas;
+
+    std::cout << "Don't forgat to buy " << apples << " apples, " << pears << " peats and " << bananas << " bananas!";
+
+}
+
