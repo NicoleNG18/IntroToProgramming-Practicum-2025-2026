2672f589080e7621addb81cfc575e98acc0c7f56
Christian Ivanov <christianivanov18@gmail.com>
2025-10-11 21:28:22 +0300

    unsigned count

--- Hello, C++/task0.cpp
+++ Hello, C++/task0.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     cout << "Hello, C++" << endl;
 
-    int appleCount, pearCount, bananaCount;
+    unsigned int appleCount, pearCount, bananaCount;
 
     cout << "Enter apple, pear and banana counts: ";
     cin >> appleCount >> pearCount >> bananaCount;
