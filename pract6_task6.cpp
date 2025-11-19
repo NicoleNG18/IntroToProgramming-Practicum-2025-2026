100ddc4a36e0011b26f45d580392a72fabf411f1
giterzieva <terzievagabriela06@abv.bg>
2025-11-19 21:51:04 +0200

    Task 6 pract 6

---
+++ pract6_task6.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <iostream>
+
+void printDiamond(int n) 
+{
+    for (int i = 1; i <= n; i++) {
+        for (int s = 1; s <= n - i; s++)
+        {
+            std::cout << " ";
+        }
+        for (int a = 1; a <= 2 * i - 1; a++)
+        {
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
+    for (int i = n - 1; i >= 1; i--) {
+        for (int s = 1; s <= n - i; s++)
+        {
+            std::cout << " ";
+        }
+        for (int a = 1; a <= 2 * i - 1; a++)
+        {
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main() 
+{
+    int n;
+    std::cin >> n;
+    printDiamond(n);
+
+    std::cout << "..........";
+}
